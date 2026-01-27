94aa5275cacda529d4ccaee619fa090c875d00f1
dcunning <31681+dcunning@users.noreply.github.com>
2026-01-27 09:04:40 -0500

    moving context middleware into the app load path
    (so we don't need to restart the server to change the layout)

--- config/initializers/rapidly_built.rb
+++ config/initializers/rapidly_built.rb
@@ -1,6 +1,4 @@
-require "tools/context_middleware"
-
 RapidlyBuilt.config do |config|
   toolkit = config.build_toolkit :tools, tools: [UiDocs::Tool]
-  toolkit.context_middleware.use Tools::ContextMiddleware
+  toolkit.context_middleware.use "ContextMiddleware"
 end
